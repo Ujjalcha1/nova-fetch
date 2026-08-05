8bc5b0b560c7c245ad644b558288058407949a65
ujjalQbent <ujjal.chatterjee@qbent.in>
2026-08-05 17:25:00 +0530

    done

--- src/layout/StatusBar.tsx
+++ src/layout/StatusBar.tsx
@@ -1,9 +1,11 @@
+import updateJson from '../../update.json'
+
 export default function StatusBar() {
   return (
     <footer className="flex h-8 items-center justify-between border-t border-white/10 bg-[#10151F] px-4 text-xs text-gray-400">
       <span>Ready</span>
 
-      <span>NovaFetch v1.0</span>
+      <span>NovaFetch v{updateJson.latestVersion}</span>
     </footer>
   )
 }
